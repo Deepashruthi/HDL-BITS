be570e353b1457247812492c32976e0417f0abab
DEEPASHRUTHI C <146473846+Deepashruthi@users.noreply.github.com>
2025-10-15 00:18:10 +0530

    Create 176.AND gate.v

---
+++ 07.Verification:Writing Testbenches/176.AND gate.v
@@ -0,0 +1,15 @@
+module top_module();
+    reg [1:0]in;
+    wire out;
+    
+    andgate uut(.in(in), .out(out));
+    
+    initial begin
+        in = 0;
+        #10 in = 1;
+        #10 in = 2;
+        #10 in = 3;
+        #10 $finish;
+    end
+
+endmodule
